0e363e88dd1a4caf1e4ef91914e712be87b74647
DeStRaaM <20destraam03@gmail.com>
2023-07-07 18:34:13 +0200

    first unit test for shop service.

--- src/app/shop.service.spec.ts
+++ src/app/shop.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ShopService } from './shop.service';
+import { Product } from './models/products.model';
 
 describe('ShopService', () => {
   let service: ShopService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ShopService],
+    });
     service = TestBed.inject(ShopService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    // Verify that no requests are outstanding
+    httpTestingController.verify();
+  });
+
+  it('should return an array of length 20', () => {
+    const category = ''; // Set the desired category if needed
+    const expectedLength = 20;
+
+    service.getProducts(category).subscribe((products) => {
+      expect(products.length).toBe(expectedLength);
+    });
+
+    const req = httpTestingController.expectOne(
+      'https://fakestoreapi.com/products/'
+    );
+    expect(req.request.method).toBe('GET');
+
+    req.flush(new Array(expectedLength).fill({} as Product)); // Simulate the response with an array of length 20
   });
 });
